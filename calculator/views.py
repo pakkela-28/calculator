78cb48d7e439c34a792d73a018ab7f120d5d64c3
divya-28 <divyapakkela28@gmail.com>
2025-07-05 11:08:02 +0530

    Add files via upload

---
+++ calculator/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+
+from django.http import HttpResponse
+def home(request):
+     result = None
+     if request.method=='POST':
+        a=int(request.POST.get('num1'))
+        b=int(request.POST.get('num2'))
+        o=request.POST.get('op')
+        if o=='add':
+            result = a+b
+        return redirect('hello',result)
+     return render(request,'home.html')
+        
+     
+def hello(request,result):
+     return render(request,'result.html',{'result':result})
+
+# Create your views here.
